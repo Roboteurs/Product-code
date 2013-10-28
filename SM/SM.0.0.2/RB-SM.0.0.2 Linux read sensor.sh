1f14c056f5b99fe2bbebea2e8c20749bef5e96dc
Reiner Schmidt <reinerschmidt@roboteurs.com>
2013-10-28 14:59:35 -0700

    initial commit

---
+++ SM/SM.0.0.2/RB-SM.0.0.2 Linux read sensor.sh
@@ -0,0 +1,17 @@
+# install i2c-tools
+sudo apt-get install i2c-tools
+
+# check the i2c buss for a device you should see the device in the output
+sudo i2cdetect -r 1
+
+# check the device ID
+sudo i2cget -f -y 1 0x6b 0x20
+
+# enable the sensor at max sampling rate
+sudo i2cset -f -y 1 0x6b 0x2a 0xff
+
+# read sensor data from adresses 0x01 - 0x06
+sudo i2cget -f -y 1 0x6b 0x28
+
+# read the temprature from adress 0x26
+sudo i2cget -f -y 0x6b 0x26
