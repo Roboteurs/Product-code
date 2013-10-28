0e9b70efbf8064065279db34af2e95cf3d069877
Reiner Schmidt <reinerschmidt@roboteurs.com>
2013-10-28 14:50:27 -0700

    initial commit

---
+++ SM/SM.0.0.1/RB-SM.0.0.1 Linux read sensor.sh
@@ -0,0 +1,14 @@
+# install i2c-tools
+sudo apt-get install i2c-tools
+
+# check the i2c buss for a device you should see the device in the output
+sudo i2cdetect -r 1
+
+# check the current sensor status, will return 0x00 if device inactive
+sudo i2cget -f -y 1 0x1c 0x0b
+
+# enable the sensor at max sampling rate
+sudo i2cset -f -y 1 0x1c 0x2a 0x01
+
+# read sensor data from adresses 0x01 - 0x06
+sudo i2cget -f -y 1 0x1c 0x01
